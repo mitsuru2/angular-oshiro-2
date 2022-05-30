324e90202115a0f794ac422016674260d65b5c11
mitsuru2 <mitsuru.takahashi@live.jp>
2022-05-30 22:58:02 +0900

    I moved the animation logic into side-menu component.

--- src/app/modules/main/main.component.ts
+++ src/app/modules/main/main.component.ts
@@ -2,20 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { NGXLogger } from 'ngx-logger';
 import { ConfirmationService } from 'primeng/api';
 import { AppInfo } from 'src/app/app-info.enum';
-import { trigger, style, state, animate, transition, AnimationEvent } from '@angular/animations';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
-  animations: [
-    // animation triggers.
-    trigger('openClose', [
-      state('open', style({ width: '15rem' })),
-      state('close', style({ width: 0 })),
-      transition('open <=> close', [animate('200ms ease-in-out')]),
-    ]),
-  ],
 })
 export class MainComponent implements OnInit {
   appInfo = AppInfo;
@@ -45,17 +36,5 @@ export class MainComponent implements OnInit {
   toggleSideMenu(): void {
     this.logger.trace('MainComponent.toggleSideMenu()');
     this.isSideMenuOpen = !this.isSideMenuOpen;
-  }
-
-  animationStart(event: AnimationEvent) {
-    if (event.toState === 'close') {
-      this.isVisible = false;
-    }
-  }
-
-  animationDone(event: AnimationEvent) {
-    if (event.toState === 'open') {
-      this.isVisible = true;
-    }
   }
 }
--- src/app/modules/main/views/side-menu/side-menu.component.ts
+++ src/app/modules/main/views/side-menu/side-menu.component.ts
@@ -1,19 +1,43 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MenuItem } from 'primeng/api';
+import { trigger, style, state, animate, transition, AnimationEvent } from '@angular/animations';
 
 @Component({
   selector: 'app-side-menu',
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.scss'],
+  animations: [
+    // animation triggers.
+    trigger('openClose', [
+      state('open', style({ width: '15rem' })),
+      state('close', style({ width: 0 })),
+      transition('open <=> close', [animate('200ms ease-in-out')]),
+    ]),
+  ],
 })
 export class SideMenuComponent implements OnInit {
-  //@Input() isOpen!: boolean;
+  @Input() isOpen!: boolean;
 
   items!: MenuItem[];
+
+  isMenuVisible!: boolean;
 
   constructor() {}
 
   ngOnInit(): void {
+    this.isMenuVisible = this.isOpen;
     this.items = [{ label: 'New' }, { label: 'List' }];
+  }
+
+  animationStart(event: AnimationEvent) {
+    if (event.toState === 'close') {
+      this.isMenuVisible = false;
+    }
+  }
+
+  animationDone(event: AnimationEvent) {
+    if (event.toState === 'open') {
+      this.isMenuVisible = true;
+    }
   }
 }
